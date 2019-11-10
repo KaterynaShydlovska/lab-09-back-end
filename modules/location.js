5c85ef0806b7a8e4a2dbab8afc11919070a93615
katerynashydlovska <shydlovskayaka@gmail.com>
2019-11-10 12:27:38 -0800

    splited js files

--- modules/location.js
+++ modules/location.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
 
 const superagent = require('superagent');
 const pg = require('pg');
-const errorHandler = require('../server.js')
+const errorHandler = require('../server.js');
+
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('err', err => { throw err; });
+
+client.connect().catch(err => {
+  throw `PG startup error ${err.message}`;
+});
 
 let locations = {};
 
@@ -13,7 +19,6 @@ function locationHandler(request, response) {
   client.query(SQL, value)
     .then(results => {
       if (results.rowCount) {
-        // console.log(results.rowCount);
         response.status(200).json(results.rows[0]);
       } else {
         const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
@@ -21,13 +26,11 @@ function locationHandler(request, response) {
           .then(data => {
             const geoData = data.body;
             const location = new Location(request.query.data, geoData);
-            // console.log(location);
             locations[url] = location;
             let locationName = location.search_query;
             let formatted_query = location.formatted_query;
             let latitude = location.latitude;
             let longitude = location.longitude;
-            console.log(locationName, latitude, longitude);
             let SQL = `INSERT INTO location (location_name, formatted_query, latitude, longitude ) VALUES ($1, $2, $3, $4) RETURNING *`;
             let safeValues = [locationName, formatted_query, latitude, longitude];
             client.query(SQL, safeValues)
@@ -54,5 +57,3 @@ function Location(query, geoData) {
 
 
 exports.locationHandler = locationHandler;
-
-
